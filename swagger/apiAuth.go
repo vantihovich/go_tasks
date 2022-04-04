2f2dafb914889b7cc8d2948dda63c08ab2f41651
Vadzim Antsikhovich <vadim.antikhovich@gmail.com>
2022-04-04 15:15:38 +0300

    Tested self-hosted swagger-ui

--- swagger/apiAuth.go
+++ swagger/apiAuth.go
@@ -14,6 +14,6 @@ var spec []byte
 func main() {
 	log.SetFlags(0)
 	http.Handle("/swagger/", http.StripPrefix("/swagger", swaggerui.Handler(spec)))
-	log.Println("serving on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("serving on :8081")
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
