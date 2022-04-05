947899013a21f3c8b8da46e52a405eb5f35ebfeb
VA <59086445+vantihovich@users.noreply.github.com>
2022-04-05 13:54:37 +0300

    Creating preliminary PR (#11)
    * Creating new folder to keep swagger tasks in it with its own .mod file
    
    * Adding preliminary commit to check if the approach works
    
    * Tested self-hosted swagger-ui
    
    * Fixed project folder structure
    
    * Fixes according to notes in the PR

---
+++ swagger/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	_ "embed"
+	"log"
+	"net/http"
+
+	"github.com/flowchartsman/swaggerui"
+)
+
+//go:embed  api\apiauth.yaml
+var spec []byte
+
+func main() {
+	log.SetFlags(0)
+	http.Handle("/swagger/", http.StripPrefix("/swagger", swaggerui.Handler(spec)))
+	log.Println("serving on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
