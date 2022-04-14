796386e987188c49e37becc317e3cf997d9dba57
Vadzim Antsikhovich <vadim.antikhovich@gmail.com>
2022-04-14 14:39:08 +0300

    One file commit

--- patterns/creational/factory/main.go
+++ patterns/creational/factory/main.go
@@ -1,18 +1,73 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	
-	"https:/github.com/vantihovich/go_tasks/tree/master/patterns/creational/factory/composer"
-	
-)
+type commonTransport interface {
+	getName() string
+	getMaxWeight() string
+	getType() string
+}
+
+type transport struct {
+	name       string
+	maxWeight  string
+	transpType string
+}
+
+func (t *transport) getName() string {
+	return t.name
+}
+
+func (t *transport) getMaxWeight() string {
+	return t.maxWeight
+}
+
+func (t *transport) getType() string {
+	return t.transpType
+}
+
+type vehicle struct {
+	transport
+}
+
+func newVehicle() commonTransport {
+	return &vehicle{
+		transport: transport{
+			name:       "TranspKings",
+			maxWeight:  "20",
+			transpType: "truck",
+		},
+	}
+}
+
+type ship struct {
+	transport
+}
+
+func newShip() commonTransport {
+	return &ship{
+		transport: transport{
+			name:       "Very Big Ship",
+			maxWeight:  "400000",
+			transpType: "ship",
+		},
+	}
+}
+
+// factory starts here :)
+func getTransport(transpType string) (commonTransport, error) {
+	if transpType == "ship" {
+		return newShip(), nil
+	} else if transpType == "truck" {
+		return newVehicle(), nil
+	} else {
+		return nil, fmt.Errorf("wrong transport type passed")
+	}
+}
 
 func main() {
 	ship, _ := getTransport("ship")
 	truck, _ := getTransport("truck")
-
-	fmt.Println("check")
 
 	printTransport(ship)
 	printTransport(truck)
