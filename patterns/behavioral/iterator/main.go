9fdd34e5b945e7bb7beb144c25ac27c2291944b8
Vadzim Antsikhovich <vadim.antikhovich@gmail.com>
2022-04-29 12:34:03 +0300

    Implemented additionally 2 behavioral patterns iterator and mediator

---
+++ patterns/behavioral/iterator/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+)
+
+type (
+	collection interface {
+		createIterator() iterator
+	}
+
+	iterator interface {
+		hasNext() bool
+		getNext() *user
+	}
+
+	userCollection struct {
+		users []*user
+	}
+
+	userIterator struct {
+		index int
+		users []*user
+	}
+
+	user struct {
+		name string
+		age  int
+	}
+)
+
+func (u *userCollection) createIterator() iterator {
+	return &userIterator{
+		users: u.users,
+	}
+}
+
+func (u *userIterator) hasNext() bool {
+	return u.index < len(u.users)
+}
+
+func (u *userIterator) getNext() *user {
+	if u.hasNext() {
+		user := u.users[u.index]
+		u.index++
+		return user
+	}
+	return nil
+}
+
+func main() {
+
+	user1 := &user{
+		name: "Stacy",
+		age:  28,
+	}
+
+	user2 := &user{
+		name: "Andy",
+		age:  25,
+	}
+
+	user3 := &user{
+		name: "Mary",
+		age:  30,
+	}
+
+	userCollection := &userCollection{
+		users: []*user{user1, user2, user3},
+	}
+
+	iterator := userCollection.createIterator()
+
+	for iterator.hasNext() {
+		user := iterator.getNext()
+		fmt.Printf("User is %+v\n", user)
+	}
+
+}
