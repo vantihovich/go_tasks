090f5534e843196727027561fd0b0a4fe252863e
VA <59086445+vantihovich@users.noreply.github.com>
2022-06-22 18:07:54 +0300

    Added user password reset functionality (#23)
    * Added user password reset functionality
    
    * Fixed the notes in PR
    
    * fixes according to the comments
    
    * fixed storing passwords as a plain text, added all necessary changes to adapt application for new passwords storing

--- swagger/models/models.go
+++ swagger/models/models.go
@@ -3,18 +3,21 @@ package models
 import "context"
 
 type User struct {
-	ID     int
-	Login  string
-	RoleID string
-	Active bool
+	ID       int
+	Login    string
+	Password string
+	RoleID   string
+	Active   bool
 }
 
 type UserRepository interface {
-	FindByLoginAndPwd(ctx context.Context, login, password string) (*User, error)
+	FindByLogin(ctx context.Context, userLogin string) (*User, error)
+	FindByID(ctx context.Context, userID int) (*User, error)
 	CheckIfLoginExists(ctx context.Context, login string) (bool, error)
 	AddNewUser(ctx context.Context, login, password, firstName, lastName, email string, socialMediaLinks []string) error
 	GetAdminAttrUserLogin(ctx context.Context, userID int) (*User, error)
 	DeactivateUser(ctx context.Context, userLogin string) (bool, error)
+	ChangePassword(ctx context.Context, userID int, newPassword string) error
 }
 
 var roleAdmin = "1"
